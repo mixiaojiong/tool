ad16b87a3fa8c6b4b2041088ecc5f50ea1bc2439
zhaorui3 <zhaorui3@le.com>
2016-06-08 15:45:18 +0800

    add install_tmux shell

---
+++ install_tmux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo yum remove -y tmux libevent libevent-devel libevent-headers
+sudo yum install -y gcc kernel-devel make ncurses-devel
+
+wget http://www.monkey.org/~provos/libevent-2.0.10-stable.tar.gz
+tar -xvzf libevent-2.0.10-stable.tar.gz
+cd libevent-2.0.10-stable
+./configure --prefix=/usr/local
+make
+sudo make install
+
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure
+--prefix=/usr/local
+make
+sudo make install
+pkill tmux
+tmux -V
