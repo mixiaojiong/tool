ea22f95c1c6afcdd02bded9414a96c082eef1e5e
zhaorui3 <zhaorui3@le.com>
2016-06-12 17:46:30 +0800

    modify

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ sudo yum install vim-enhanced
 sudo yum install -y zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 # tmux
-sh install_tmux.sh
+sudo sh install_tmux.sh
 # vim
-cp .vimrc ~/.vimrc
-cp .vimrc.bundles ~/.vimrc.bundles
+ln .vimrc ~/.vimrc
+ln .vimrc.bundles ~/.vimrc.bundles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
